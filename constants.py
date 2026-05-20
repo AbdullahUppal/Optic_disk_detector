d903c8ed4ea542e8ef81c85d816d10b89eb6e336
Abdullah Uppal <au00152@gmail.com>
2026-05-20 09:11:26 -0700

    Added basic imports and constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv
+
+FUNDUS_IMAGES_PATH=os.getenv("FUNDUS_IMAGES_PATH")
+VESSEL_STRUCTURE_PATH=os.getenv("VESSEL_STRUCTURE_PATH")
+RESULT_CSV_PATH=os.getenv("RESULT_CSV_PATH")
---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+import os
+
+if __name__ == "__main__":
+    retina_imgs = cv.load()
+
