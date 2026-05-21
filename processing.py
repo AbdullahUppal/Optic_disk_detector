d12e9af372e115e72d75c5ff78ee879784f80bbc
Abdullah Uppal <au00152@gmail.com>
2026-05-21 08:55:30 -0700

    Added the candidate area identification in the after preprocessing

--- processing.py
+++ processing.py
@@ -1,13 +1,16 @@
 import cv2 as cv
+import numpy as np
 
-def pre_process(image, resize=None):
+def pre_process(image, vessel, resize=None):
    
     if resize:
         image = resize_image(image)
+        vessel = resize_image(vessel)
 
     equalized_img = cv.equalizeHist(image)
+    equalized_vessel = cv.equalizeHist(vessel)
 
-    return equalized_img
+    return equalized_img, equalized_vessel
 
 def resize_image(image):
     width ,height = image.shape[:2]
@@ -21,22 +24,41 @@ def resize_image(image):
 
 def identify_candidate_area(image, vessel_map):
     
-    ret, updated_img = cv.threshold(image, 255, type=cv.THRESH_OTSU)
-    cv.imshow("thresholded_img", updated_img)
-    cv.waitKey(0)
+    _, updated_img = cv.threshold(image, 245, 255, type=cv.THRESH_BINARY)
+    _, updated_vessel = cv.threshold(vessel_map, 80, 255, type=cv.THRESH_BINARY)
+
+    candidate_area = cv.bitwise_and(updated_img, updated_vessel)
+    cv.imshow("canddate_region", candidate_area)
+    cv.waitKey()
+
+    # testing for best threshold
+    # cv.imshow("original_img", image)
+    # cv.imshow("thresholded_img", updated_img)
+    # cv.imshow("orig_vessel", vessel_map)
+    # cv.imshow("threshold_vessel", updated_vessel)
+    # cv.waitKey(0)
 
 if __name__ == "__main__":
 
-    RESIZE = 0
+    RESIZE = 1
 
-    image = cv.imread("C:/Users/au001/Documents/Projects\ML DIP/optic_disk_detector/Fundus image/01ffa92e4-8d87-11e8-9daf-6045cb817f5b..JPG")
+    vessel = cv.imread("Blood vessels/1ffa92e4-8d87-11e8-9daf-6045cb817f5b._bin_seg.png", cv.IMREAD_GRAYSCALE)
 
-    vessel = cv.imread("C:/Users/au001/Documents/Projects\ML DIP/optic_disk_detector/Blood vessels/1ffa92e4-8d87-11e8-9daf-6045cb817f5b._bin_seg.png")
+    image = cv.imread("Fundus image/01ffa92e4-8d87-11e8-9daf-6045cb817f5b..JPG")
+
+    # best possible channel for further processing 
+    # cv.imshow("Image",image)
+    # cv.imshow("ImageC1", image[:,:,0])
+    # cv.imshow("ImageC2", image[:,:,1])
+    # cv.imshow("ImageC3",image[:,:,2])
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+    image = image[:,:,2]
 
     if RESIZE:
         vessel = resize_image(vessel)
 
-    processed_img = pre_process(image, resize=RESIZE)
+    processed_img, processed_vessel = pre_process(image, vessel, resize=RESIZE)
 
     identify_candidate_area(image=processed_img, vessel_map=vessel)
 
