38a77c959e83058befe74a9667e8eb5107a20ace
Abdullah Uppal <au00152@gmail.com>
2026-05-23 11:06:04 -0700

    completed the implementation of identifying the central area and marking it.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import FUNDUS_IMAGES_PATH, VESSEL_STRUCTURE_PATH
 
 
 
+
 if __name__ == "__main__":
     retina_files = os.listdir(FUNDUS_IMAGES_PATH)
     vessel_files = os.listdir(VESSEL_STRUCTURE_PATH)
--- processing.py
+++ processing.py
@@ -28,8 +28,21 @@ def identify_candidate_area(image, vessel_map):
     _, updated_vessel = cv.threshold(vessel_map, 80, 255, type=cv.THRESH_BINARY)
 
     candidate_area = cv.bitwise_and(updated_img, updated_vessel)
-    cv.imshow("canddate_region", candidate_area)
-    cv.waitKey()
+    
+    # cv.imshow("Candidate Image", candidate_area)
+    final_img = identify_central_area(candidate_area)
+
+    # cv.imshow("Final Image", final_img)
+    # cv.waitKey()
+    
+    contours, _ = cv.findContours(final_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        moments = cv.moments(contour)
+        if moments["m00"] != 0:
+            cx = int(moments["m10"] / moments["m00"])
+            cy = int(moments["m01"] / moments["m00"])
+
+    return cx,cy
 
     # testing for best threshold
     # cv.imshow("original_img", image)
@@ -39,18 +52,41 @@ def identify_candidate_area(image, vessel_map):
     # cv.waitKey(0)
 
 # Update it's implementation to get biggest white ares.
-    def ccaf(imgstr, Vset, value):
-        img1 = cv.imread(imgstr, 0)
-        label = np.zeros((img1.shape[0], img1.shape[1], 3), dtype='int')
-        # label = cv.imread(imgstr)
-        for x in range(0, img1.shape[0]-1):
-            for y in range(0, img1.shape[1]-1):
-                if img1[x][y] in Vset:
-                    if img1[x-1][y] in Vset or img1[x][y-1] in Vset:
-                        label[x][y] = value
-                    else:
-                        label[x][y] = value
-        return np.uint8(label)
+def identify_central_area(img1):
+    Vset = 255
+    label = {}
+    count = 1
+    # label = cv.imread(imgstr)
+    for x in range(1, img1.shape[0] - 1):
+        for y in range(1, img1.shape[1] - 1):
+            if img1[x][y] == Vset:
+                if (
+                    img1[x - 1][y] == Vset or
+                    img1[x][y - 1] == Vset or
+                    img1[x - 1][y - 1] == Vset or
+                    img1[x - 1][y + 1] == Vset
+                ):
+                    label[f"area_{count}"].append([x, y])
+                else:
+                    count += 1
+                    label[f"area_{count}"] = []
+                    label[f"area_{count}"].append([x, y])
+    # print(label)
+    largest = 0
+    largest_key = ""
+    for key, value in label.items():
+        # print(value)
+        var = len(value)
+        if var > largest:
+            largest_key = key
+            largest = var
+    final_area_img = np.zeros((img1.shape[0], img1.shape[1]), dtype=np.uint8)
+    print(label[largest_key])
+    for x, y in label[largest_key]:
+        final_area_img[x][y] = 255
+
+    return np.uint8(final_area_img)
+
     
 if __name__ == "__main__":
 
@@ -67,14 +103,29 @@ if __name__ == "__main__":
     # cv.imshow("ImageC3",image[:,:,2])
     # cv.waitKey(0)
     # cv.destroyAllWindows()
-    image = image[:,:,2]
 
     if RESIZE:
         vessel = resize_image(vessel)
+        image_rbg = resize_image(image)
+
+    image = image[:,:,2]
 
     processed_img, processed_vessel = pre_process(image, vessel, resize=RESIZE)
 
-    identify_candidate_area(image=processed_img, vessel_map=vessel)
+    cx, cy = identify_candidate_area(image=processed_img, vessel_map=vessel)
+
+    cv.drawMarker(
+        image_rbg,
+        (cx, cy),
+        (0, 0, 0),
+        markerType=cv.MARKER_CROSS,
+        markerSize=10,
+        thickness=2,
+        line_type=cv.LINE_AA,
+    )
+    cv.imshow("circled image", image_rbg)
+    cv.waitKey()
+
 
 
 
